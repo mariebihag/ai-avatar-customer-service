096ba0160f14ef9fa7f01a1c1ea8545aa8d37d76
mariebihag <mkazser@gmail.com>
2026-02-12 21:09:38 +0800

    Fix Vite base path for Render

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  
   plugins: [react()],
-  base: '/ai-avatar-customer-service/',
-  build: {
-    chunkSizeWarningLimit: 2000, // suppress warning only
-  }
+  base: './',   // ✅ ensures assets load correctly on Render
 })
